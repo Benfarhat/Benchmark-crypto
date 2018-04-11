381c48400d90a8c1f8545a6c1b4d1c5c3ac36c85
Benfarhat <benfarhat.elyes@gmail.com>
2018-04-11 14:08:37 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var CryptoJS = require('crypto-js')
-var fs = require('fs');
-var leftpad = require('leftpad');
+var fs = require('fs')
+var leftpad = require('leftpad')
 
-var start = Date.now();
-var limit = 99999999;
+var start = Date.now()
 
 
 var filename = "generate.txt"
@@ -13,33 +12,64 @@ const defaults = {
     fd: null,
     mode: 0o666,
     autoClose: true
-  };
-var stream = fs.createWriteStream(filename, defaults);
+  }
+var stream = fs.createWriteStream(filename, defaults)
 
+fd = fs.openSync(filename, 'a')
 
+var configs = {
+    methods : {
+        SHA256 : true,
+        HS : false,
+    },
+    options : {
+        limit : 99999999,
+        key : "Le5d4f69g1sfkb54g43f7ap9",
+        console : false,
+        writeInFile : false,
+        mod: 10000
+    }   
+}
 
-
-fd = fs.openSync(filename, 'a');
 
 stream.once('open', function(fd) {
     // Starting
-    start = Date.now();
-    console.log("starting");
+    start = Date.now()
+    console.log("Starting")
     
-    // Loop
-    let i = 0; 
-    for(; i <= limit; i++){
-        let word = leftpad(i, 8);
-        let generate = CryptoJS.SHA256(word).toString(CryptoJS.enc.utf8).toUpperCase()
-        stream.write(word + ":" + generate + "\n");
+    if(configs.methods.SHA256){
+        stream.write("\n---==============================SHA256==============================---\n\n")
+
+        let i = 0; 
+        for(; i <= configs.options.limit; i++){
+            let word = leftpad(i, 8)
+            let generate = CryptoJS.SHA256(word).toString(CryptoJS.enc.utf8).toUpperCase()
+            if (configs.options.writeInFile) stream.write(leftpad(i, 8) + ":" + generate + "\n")
+            if (configs.options.console) console.log(leftpad(i, 8) + ":" + generate + "\n")
+            
+            if (i % configs.options.mod === 0) console.log(i)
+        } 
+    }
+    
+    if(configs.methods.HS){
+        stream.write("\n---=============================HmacSHA1=============================---\n\n")
+
+        let i = 0; 
+        for(; i <= configs.options.limit; i++){
+            let word = leftpad(i, 8)
+            let generate = CryptoJS.HmacSHA1(word, key).toString(CryptoJS.enc.utf8).toUpperCase()
+            if (configs.options.writeInFile) stream.write(leftpad(i, 8) + ":" + generate + "\n")
+            if (configs.options.console) console.log(leftpad(i, 8) + ":" + generate + "\n")
+            
+            if (i % configs.options.mod === 0) console.log(i)
+        } 
     }
 
     // Finished
     let message = `Finished in: ${ ( Date.now() - start ) / 1000 } seconds`
-    stream.write(message);
-    stream.end();
+    if (configs.options.writeInFile) {
+        stream.write(message)
+        stream.end()
+    }
     console.log(message)
-}); 
-
-
-
+});
