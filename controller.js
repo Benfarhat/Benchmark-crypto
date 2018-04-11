4318c9bfb61138e433441270cdf3be97ba2544ea
Benfarhat <benfarhat.elyes@gmail.com>
2018-04-12 00:35:56 +0100

    Adding drop collection and readline

--- controller.js
+++ controller.js
@@ -7,8 +7,7 @@ module.exports = word => {
   Tuples.save((err, saved) => {
     if (err) {
         if (err.code === 11000) { //error code for duplicate
-          // message: 'E11000 duplicate key error collection: cryptoDB.correspondances index: identity_1 dup key: { : "dup" }'
-          console.error("This correspondance already exists: " + err.message.split(':')[2])
+          console.error("This correspondance already exists: " + err.message)
         }            
     }
   })
--- index.js
+++ index.js
@@ -3,27 +3,50 @@ const Combinatorics = require('js-combinatorics')
 const mongoose = require('mongoose')
 const Correspondance = require('./model')
 const SaveCorrespondance = require('./controller')
+const readline = require('readline')
+
+// Required for user confirmation on drop collection
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 // Mongoose stuff
 const mongoDBURL = 'mongodb://127.0.0.1/cryptoDB';
 mongoose.connect(mongoDBURL)
-  .then(() =>  console.log(`connection to ${mongoDBURL} succesful`))
+  .then(() =>  {
+    console.log(`\n> Connection to ${mongoDBURL} succesful`)
+
+    if(configs.reset){
+      rl.question('\n> Confirm that you want to drop "correspondances" collection? Y/N: ', (answer) => {
+        if(answer == 'Y' || answer == 'y')
+          mongoose.connection.dropCollection('correspondances', (err) => {
+          if (err) {
+            return err
+          }
+          console.log("Collection dropped!")
+        });
+        
+        rl.close();
+      });
+    
+    }
+  })
 .catch((err) => console.error(err));
 
 // 
 const configs = {
   alphabets : [0,1,2,3,4,5,6,7,8,9], // Alphabets that will create words
-  wordSize : 2, // The input word size, for ex: 012345 -> 6
+  wordSize : 4, // The input word size, for ex: 012345 -> 6
   reset : false, // Delete collections (Correspondance Dictionnary ),
   information : true // Display some informations on console
 }
 
+
+
 // Create all combinaisons with repetition
 const vocabularies = Combinatorics.baseN(configs.alphabets,configs.wordSize)
 
-if(configs.reset){
-  mongoose.connection.db.dropCollection('Correspondance');
-}
 if(configs.information){
   console.log('---================================================---')
   console.log("   > Alphabets: " + configs.alphabets)
@@ -31,6 +54,8 @@ if(configs.information){
   console.log("   > Number of combinaisons with repetitions: " + vocabularies.length)
   console.log('---================================================---')
 }
-//vocabularies.map(element => {SaveCorrespondance(element.join(''))})
+
+
+vocabularies.map(element => {SaveCorrespondance(element.join(''))})
 
 /* Crypto and leftpad stuff are inside mongoose save hook: @see ./model.js */
